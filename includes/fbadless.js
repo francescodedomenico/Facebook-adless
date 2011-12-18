9dba5458cd3eaa095dddfb9d323710b3020d9004
Francesco De Domenico <frankdd89@hotmail.it>
2011-12-18 16:18:09 +0100

    first release commit

---
+++ includes/fbadless.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @include http://*facebook.com/*
+// @include https://*facebook.com/*
+// ==/UserScript==
+
+var oexFacebookadless = function()
+{
+  
+  window.addEventListener('DOMContentLoaded', function()
+  {
+    function removeElementByID(id)
+    {
+      var elem = document.getElementById(id);
+      if (elem != null) elem.parentNode.removeChild(elem);
+    }
+	/* Insert elements id to remove here */
+	removeElementByID('pagelet_ego_pane_w');
+	removeElementByID('pagelet_side_ads');
+	removeElementByID('pagelet_ego_pane');
+	/* change app side scrollbar activity */
+	var elem = document.getElementById("pagelet_ticker");
+	
+    if (elem != null) {
+		var childs = elem.childNodes;
+		var childsNew = childs[0].childNodes;
+		var ad =childsNew[2];
+		if(ad!=null) ad.style.height="576px";
+	}
+  },false);
+  
+}();
+
