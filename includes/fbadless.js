8a6b9d6ece680e51d17b02834e22651f1b6c1766
Francesco De Domenico <frankdd89@hotmail.it>
2012-02-09 21:20:34 +0100

    removes ads in fbPhotoSnowboxAdsSide now

--- includes/fbadless.js
+++ includes/fbadless.js
@@ -23,6 +23,9 @@ var oexFacebookadless = function()
 	var th = document.getElementById("fbPhotoSnowboxAdsBottom");
 	if (th != null) th.parentNode.style.visibility="hidden";
 	
+	th = document.getElementById("fbPhotoSnowboxAdsSide");
+	if (th != null) th.parentNode.style.visibility="hidden";
+	
 	/* change app side scrollbar activity */
 	var elem = document.getElementById("pagelet_ticker");
     if (elem != null) {
