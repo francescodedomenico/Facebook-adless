e61325f6f737129c57d8585e19a1dbc9b0587bad
Francesco De Domenico <frankdd89@hotmail.it>
2012-08-24 18:25:55 +0200

    removed the new ads box in theater mode

--- includes/fbadless.js
+++ includes/fbadless.js
@@ -16,11 +16,16 @@ var oexFacebookadless = function()
 	"fbPhotoSnowliftAdsSide",
 	"fbPhotoSnowboxAdsSide"
 	);
+	
+//jsDOMTools v0.1
+//author: Francesco De Domenico
+//website: https://github.com/frankdd89/jsDOMTools
+function removeElementByID(b){var a=document.getElementById(b);if(a!=null){a.parentNode.removeChild(a)}}function removeElementByClassName(d,e){var b=document.getElementsByTagName(d);if(b!=null){for(var c=0,a=b.length;c<a;c++){if(b[c].getAttribute("class")==e){b[c].parentNode.removeChild(b[c])}}}}function hideElementByClassName(d,e){var b=document.getElementsByTagName(d);if(b!=null){for(var c=0,a=b.length;c<a;c++){if(b[c].getAttribute("class")==e){b[c].style.visibility="hidden"}}}};
   
   window.addEventListener('DOMNodeInserted', function()
   {
 	var temp = null;
-	
+	hideElementByClassName("div","-cx-PRIVATE-snowliftAds__root rhcFooter");
 	for (var i=0; i<ads.length; i++){
 		temp = document.getElementById(ads[i]);
 		if(temp != null){
